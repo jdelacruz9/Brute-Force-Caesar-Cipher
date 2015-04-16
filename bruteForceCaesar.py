7c0dc494a94d944aea95853eaa4c1439f1bdde9a
Julio de la Cruz <jjdl_cn@hotmail.com>
2015-04-15 22:41:55 -0400

    Version without useless comments

--- bruteForceCaesar.py
+++ bruteForceCaesar.py
@@ -6,10 +6,7 @@ def valid(phrase):
     sizeH = len(phrase)/2
     counter = 0
     for word in phrase:
-        # if(counter > sizeH):
-            # return True
         if word.lower() in dictionary:
-            # print word
             counter = counter + 1
     if(counter > sizeH):
         return True
@@ -23,7 +20,6 @@ def makeOneLine(phrase):
         result = result + item
 
     return result
-    # return result.replace("'", "")
 
 def decaesar(message):
     for n in range(1,27):
@@ -41,19 +37,9 @@ def decaesar(message):
             else:
                 newMsg = newMsg + letter
         msgList = newMsg.split(' ')
-        # print msgList[0]
-        # valid(msgList)
         if(valid(msgList)):
-            # print newMsg
-            print "%s: %s"%(n,newMsg)   
-        # if((msgList[0].lower() in dictionary) or (msgList[1].lower() in dictionary)):
-            # print newMsg
-        # print "%s: %s"%(n,newMsg)              
+            print "%s: %s"%(n,newMsg)            
 
-
-# print "Where".lower() in dictionary
-# print dictionary.split('\n')
-# print 'dolyl' in dictionary
 msg1 = """Bmjwj mzrtw nx htshjwsji ymjwj fwj st xyfsifwix -- st tsj hfs xfd bmfy
 nx ltti tw gfi, fqymtzlm dtz hfs gj xzwj ymfy jajwdtsj bnqq.
         -- Otms Pjssjym Lfqgwfnym"""
@@ -64,12 +50,6 @@ msg2 = """Ivnpsjtut bmxbzt tju bu uif dijmesfo't ubcmf.
 msg3 = """Pa pzu'a uljlzzhyf av ohcl ylshapclz pu Rhuzhz Jpaf pu vykly av il buohwwf.
         -- Nyvbjov Thye"""
 
-
-msgRicky = """ O ngbk g sgv ul znk Atozkj Yzgzky.  Oz'y gizagr yofk.  O yvktz rgyz yasskx
-lurjotm oz.  Vkuvrk gyq sk cnkxk O robk, gtj O yge, "K6".
-        -- Yzkbkt Cxomnz"""
-
-#5 1 7
 decaesar(makeOneLine(msg1))
 print "-----------------------------------------"
 print 
@@ -80,7 +60,7 @@ decaesar(makeOneLine(msg3))
 print "-----------------------------------------"
 print 
 
-decaesar(makeOneLine(msgRicky))
+# decaesar(makeOneLine(msgRicky))
 
 #5 1 7
 
